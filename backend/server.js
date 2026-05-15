f71933e5f80474d36a21ed4019797a2cac52ec63
Ruchii Dhawale <your_email@example.com>
2026-05-15 12:57:59 +0530

    Register API completed

--- backend/server.js
+++ backend/server.js
@@ -15,40 +15,6 @@ app.get("/", (req, res) => {
     res.send("Backend Running Successfully");
 });
 
-app.post("/register", async (req, res) => {
-
-    const { name, email, password, address } = req.body;
-
-    try {
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const sql = `
-            INSERT INTO users(name, email, password, address)
-            VALUES (?, ?, ?, ?)
-        `;
-
-        db.query(
-            sql,
-            [name, email, hashedPassword, address],
-            (err, result) => {
-
-                if(err){
-                    return res.status(500).json(err);
-                }
-
-                res.json({
-                    message: "User Registered Successfully"
-                });
-            }
-        );
-
-    } catch(error){
-        res.status(500).json(error);
-    }
-
-});
-
 app.listen(5000, () => {
     console.log("Server Running on Port 5000");
 });
